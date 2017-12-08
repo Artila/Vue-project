0685bacaea1f6f2df199d49c3e52598a81ebc57a
Artila <529276960@qq.com>
2017-12-08 16:49:32 +0800

    入口

---
+++ Quotes/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on a Quote to delete it!
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+  import Header from './components/Header.vue';
+
+  export default {
+    data: function () {
+      return {
+        quotes: [
+          'Just a Quote to see something'
+        ],
+        maxQuotes: 10
+      }
+    },
+    methods: {
+      newQuote(quote) {
+        if (!quote) {
+          return;
+        }
+        if (this.quotes.length >= this.maxQuotes) {
+          return alert('You have reached the limit of ' + this.maxQuotes + ' quotes. Please remove some items from the list before adding more.');
+        }
+        this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header
+    }
+  }
+</script>
+
+<style>
+</style>
