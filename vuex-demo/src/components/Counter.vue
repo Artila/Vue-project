5309743726b8a3c32e099e213cb3656e80f226cc
Artila <529276960@qq.com>
2017-12-09 22:35:16 +0800

    vuex的小demo

---
+++ vuex-demo/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<button class="btn btn-primary" @click="increment">increment</button>
+		<button class="btn btn-primary" @click="decrement">decrement</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			increment() {
+				this.$emit('updated', 1);
+			},
+			decrement() {
+				this.$emit('updated', -1);
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
