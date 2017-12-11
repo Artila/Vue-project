107914b7477abfab724c6e76176aaa0ac1fdffe3
Artila <529276960@qq.com>
2017-12-11 16:01:14 +0800

    mapGetters

---
+++ vuex-demo/src/components/AnotherResult.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<p>DoubleCounter is {{ doubleCounter }}</p>
+		<p>Number of clicks: {{ clickCounter }}</p>
+	</div>
+</template>
+
+<script>
+	import { mapGetters } from 'vuex';
+	
+	export default {
+		computed: {		
+//			counter() {
+//				// 1. 获取state
+//				// return this.$store.state.counter * 2;
+//				// 2. 获取getter
+//				return this.$store.getters.doubleCounter;
+//			},
+//			clicks() {
+//				return this.$store.getters.clickCounter;
+//			}
+			...mapGetters([
+				'doubleCounter',
+				'clickCounter'
+			])
+		}
+	}
+</script>
+
+<style>
+</style>
