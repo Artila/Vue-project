20cda60db811f89c5d0c2dae15dfd2010fa283fb
Artila <529276960@qq.com>
2017-12-11 16:01:42 +0800

    v-model和vuex

--- vuex-demo/src/App.vue
+++ vuex-demo/src/App.vue
@@ -3,9 +3,14 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 				<h1>Vuex</h1>
-				<app-result :counter="counter"></app-result>
+				<app-result></app-result>
+				<app-another-result></app-another-result>
 				<hr />
-				<app-counter @updated="counter += $event"></app-counter>
+				<app-counter></app-counter>
+				<app-another-counter></app-another-counter>
+				<hr />
+				<input type="text"  v-model="value">
+				<p>{{ value }}</p>
 			</div>
 		</div>
 	</div>
@@ -13,18 +18,38 @@
 
 <script>
 	import Counter from './components/Counter.vue';
+	import AnotherCounter from './components/AnotherCounter.vue';
 	import Result from './components/Result.vue';
-	
+	import AnotherResult from './components/AnotherResult.vue';
 	
 	export default {
-		data() {
-			return {
-				counter: 0
+		// 第一种： 把vuex和v-model绑定的方法：:value="value" @input="updateValue"
+//		computed: {
+//			value() {
+//				return this.$store.getters.value;
+//			}
+//		},
+//		methods: {
+//			updateValue(event) {
+//				return this.$store.dispatch('updateValue', event.target.value);
+//			}
+//		},
+		// 第二种
+		computed: {
+			value: {
+				get() {
+					return this.$store.getters.value;
+				},
+				set(value) {
+					return this.$store.dispatch('updateValue', value);
+				}
 			}
 		},
 		components: {
 			appCounter: Counter,
-			appResult: Result
+			appAnotherCounter: AnotherCounter,
+			appResult: Result,
+			appAnotherResult: AnotherResult
 		}
 	}
 </script>
